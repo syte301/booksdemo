fe461911dccdc99190d99baa6cfb89a4d601bedd
ww301347 <ww301347@gmail.com>
2023-02-25 00:54:16 +0600

    attr_read commit

--- app/services/directory_list.rb
+++ app/services/directory_list.rb
@@ -1,12 +1,10 @@
 #app/services/directory_list.rb
 
 class DirectoryList
+  attr_reader :files
+  
   def initialize(path)
     @files = Dir.glob(path).select { |filename| File.file?(filename) }.map { |fullpath| File.basename fullpath }.sort
-  end
-
-  def files
-    @files
   end
 end
 
